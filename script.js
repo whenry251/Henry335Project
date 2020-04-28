18e447da256d3ed71f7e965516065712b477caa1
Whitney Henry <54414134+whenry251@users.noreply.github.com>
2020-04-27 21:48:04 -0500

    addition of pdfs, client info, cleaning up on design issues, etc.

--- script.js
+++ script.js
@@ -10,4 +10,16 @@ $(document).ready(function (){
     else if (hour >= 16 && hour <= 24){greeting = "GOOD EVENING";}
     
     document.getElementById("visitGreeting").innerHTML = greeting + "!";
+    
+    
+    /*I literally tried for hours to write a script-based slideshow for the design page but it was not working at ALL, so I decided to learn how to make a bootstrap one off of W3 instead.
+    var imageList = ["slideshow/ashlyn.png", "slideshow/meredith.png", "slideshow/frostystickers.JPG", "slideshow/bearcatfoodpantry.png", "slideshow/asbsticker.png", "slideshow/asbinfosess.png", "slideshow/asbcorona1.jpg", "slideshow/asbcorona.gif", "slideshow/asbexecannounce.png"];
+    var x = 0;*/
+    
+    
+    
     });
+
+
+
+
