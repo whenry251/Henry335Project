13e9b544bb63f0dd160025138fbd119e36e0f0b0
Whitney Henry <54414134+whenry251@users.noreply.github.com>
2020-05-01 01:09:56 -0500

    more interactivity

--- script.js
+++ script.js
@@ -1,18 +1,6 @@
 $(document).ready(function (){
     $('#fade').css('display', 'none').fadeIn(1000);
     
-    $(".regButton").mouseover(function(){
-        $(this).css("background-color", "#e0e0e0");
-    });
-    
-    $(".regButton").mouseout(function(){
-        $(this).css("background-color", "silver");
-    });
-    
-    $("#submitBtn").click(function(){
-        alert("A new window/tab will now open to submit your form via email. Click OK to continue.")
-    });
-    
     var myDate = new Date();
     var hour = myDate.getHours();
     var greeting;
@@ -22,9 +10,23 @@ $(document).ready(function (){
     else if (hour >= 16 && hour <= 24){greeting = "GOOD EVENING";}
     
     document.getElementById("visitGreeting").innerHTML = greeting + "!";
-    
     });
 
+$(".regButton").mouseover(function(){
+        $(this).css("background-color", "#e0e0e0");
+    });
+    
+    $(".regButton").mouseout(function(){
+        $(this).css("background-color", "silver");
+    });
+    
+    $("#submitBtn").click(function(){
+        alert("A new window/tab will now open to submit your form via email. Click OK to continue.");
+    });
+
+document.getElementById("moreSoon").onclick = function noSeriously (){
+       document.getElementById("moreSoon").innerHTML = "NO, SERIOUSLY. THAT'S IT.";
+   };
 
 
 
